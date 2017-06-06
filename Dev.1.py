c3091048af61fe67eed01c321bae10fff85b5bde
JRBenson01 <mc.nyancat5400@gmail.com>
2017-06-05 20:41:36 -0400

    Uploading files from local computer

---
+++ Dev.1.py
@@ -0,0 +1,2 @@
+from easygui import *
+buttonbox(msg="Sustainia Full map", title="Sustainia", choices=("15", "12"), image="sus.png")
---
+++ intbox.py
@@ -0,0 +1,5 @@
+from easygui import *
+menu = integerbox(msg="Sustainia Main Menu", title="Sustainia", lowerbound=1, upperbound=30, image="sus.png")
+if menu == 15:
+    buttonbox(msg="Territory 15, Montemnix THE ABSOLUTE BEST!!", title="Sustainia", choices=("Back", "Trade"))
+
